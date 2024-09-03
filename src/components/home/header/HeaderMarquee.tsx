9e123fdfa89162f7f3c019deb71533f3e3bef53a
Tim Fuhrmann <tim.fuhrmann@deptagency.com>
2024-09-03 12:36:40 +0200

    Fix marquee

--- src/components/home/header/HeaderMarquee.tsx
+++ src/components/home/header/HeaderMarquee.tsx
@@ -8,17 +8,17 @@ type HeaderMarqueeProps = {
   initialLength: number;
 };
 export const HeaderMarquee = ({ text, initialLength }: HeaderMarqueeProps) => (
-  <div className={cn(styles.marquee, "flex items-center gap-[--gap] overflow-hidden")}>
+  <div className="flex items-center overflow-hidden">
     {times(
       (index) => (
         <div
           key={index}
-          className={cn(styles.marqueeAnimation, "flex items-center gap-[--gap] whitespace-nowrap")}
+          className={cn(styles.marqueeAnimation, "flex items-center whitespace-nowrap")}
         >
           {times(
             (index) => (
               <Fragment key={index}>
-                {text}
+                <span className="px-4">{text}</span>
                 <span className="inline-block size-[.3em] shrink-0 rounded-full bg-foreground-1" />
               </Fragment>
             ),
